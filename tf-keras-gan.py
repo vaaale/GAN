d8e36ef34612914ce169a942da26cf63e31422e5
alex <alexander.vaagan@gmail.com>
2017-10-15 13:18:27 +0200

    New model

--- tf-keras-gan.py
+++ tf-keras-gan.py
@@ -1,23 +1,31 @@
+import tensorflow.contrib.keras.api.keras.backend as K
 import tensorflow as tf
 from tensorflow.contrib.keras.python.keras import Input
+from tensorflow.contrib.keras.python.keras.datasets import cifar10
 from tensorflow.contrib.keras.python.keras.models import Model
 from tensorflow.contrib.keras.python.keras.layers import Dense, Activation, concatenate, Convolution2D, UpSampling2D, \
-    Flatten, Reshape
+    Flatten, Reshape, AveragePooling2D
+from tensorflow.contrib.keras.python.keras.layers.normalization import BatchNormalization
+from tensorflow.contrib.keras.python.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.contrib.keras.python.keras.utils import to_categorical
 from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import os
 
+# mnist = input_data.read_data_sets('../../MNIST_data', one_hot=True)
 
-mnist = input_data.read_data_sets('../../MNIST_data', one_hot=True)
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
 mb_size = 64
-Z_dim = 100
+Z_dim = 200
 # X_dim = mnist.train.images.shape[1]
-X_dim = (28, 28, 1)
-y_dim = mnist.train.labels.shape[1]
+X_dim = (32, 32, 3)
+# y_dim = mnist.train.labels.shape[1]
+y_dim = 10
 h_dim = 128
 
+K.set_learning_phase(True)
 
 def xavier_init(size):
     in_dim = size[0]
@@ -25,62 +33,43 @@ def xavier_init(size):
     return tf.random_normal(shape=size, stddev=xavier_stddev)
 
 
-""" Discriminator Net model """
-
-
+# Discriminator Net model
 def discriminator(X_dim, y_dim):
     x_in = Input(shape=(X_dim), name='X_input')
     y_in = Input(shape=(y_dim,), name='Y_input')
-    D_h1 = Convolution2D(50, kernel_size=(2, 2), padding='same', activation='relu')(x_in)
-    D_h2 = Convolution2D(50, kernel_size=(2, 2), padding='same', activation='relu')(D_h1)
-    D_f = Flatten()(D_h2)
-    merge = concatenate([D_f, y_in])
-    D_d1 = Dense(100, activation='relu')(merge)
-    D_logit = Dense(1)(D_d1)
-    D = Model(inputs=[x_in, y_in], outputs=D_logit)
-
+    D_h = Activation('relu')(Convolution2D(128, kernel_size=(5, 5), padding='same')(x_in))
+    D_h = Activation('relu')(BatchNormalization()(Convolution2D(128, kernel_size=(5, 5), padding='same')(D_h)))
+    D_h = Activation('relu')(BatchNormalization()(Convolution2D(256, kernel_size=(5, 5), padding='same')(D_h)))
+    D_h = Activation('relu')(BatchNormalization()(Convolution2D(512, kernel_size=(5, 5), padding='same')(D_h)))
+    D_h = AveragePooling2D(pool_size=(2, 2), padding='valid')(D_h)
+    D_h = Flatten()(D_h)
+    D_h = concatenate([D_h, y_in])
+    D_logit = Dense(1, name='Discriminator_Output')(D_h)
+    D = Model(inputs=[x_in, y_in], outputs=D_logit, name='Discriminator')
+    print('=== Discriminator ===')
+    D.summary()
+    print('\n\n')
     return D
 
-# def discriminator(X_dim, y_dim):
-#     x_in = Input(shape=(X_dim,), name='X_input')
-#     y_in = Input(shape=(y_dim,), name='Y_input')
-#     inputs = concatenate([x_in, y_in])
-#     D_h1 = Dense(h_dim, activation='relu')(inputs)
-#     D_logit = Dense(1)(D_h1)
-#
-#     D = Model(inputs=[x_in, y_in], outputs=D_logit)
-#
-#     return D
-#
 
-""" Generator Net model """
-
-
-def dense_generator(Z_dim, y_dim):
-    z_in = Input(shape=(Z_dim,), name='Z_input')
-    y_in = Input(shape=(y_dim,), name='y_input')
-    inputs = concatenate([z_in, y_in])
-    G_h1 = Dense(h_dim, activation='relu')(inputs)
-    G_prob = Dense(X_dim, activation='sigmoid')(G_h1)
-
-    G = Model(inputs=[z_in, y_in], outputs=G_prob)
-
-    return G
-
-
+# Generator Net model
 def generator(Z_dim, y_dim):
     z_in = Input(shape=(Z_dim,), name='Z_input')
     y_in = Input(shape=(y_dim,), name='y_input')
     inputs = concatenate([z_in, y_in])
-    G_h1 = Dense(100*14*14, activation='relu')(inputs)
-    G_reshaped = Reshape(target_shape=(14, 14, 100))(G_h1)
-    G_up = UpSampling2D(size=(2, 2))(G_reshaped)
-    G_c1 = Convolution2D(50, kernel_size=(2, 2), padding='same', activation='relu')(G_up)
-    G_prob = Convolution2D(1, kernel_size=(1, 1), padding='same', activation='sigmoid')(G_c1)
-    # G_prob = Flatten()(G_c2)
+    G_h = Dense(1024 * 32 * 32, activation='relu')(inputs)
+    G_h = Reshape(target_shape=(32, 32, 1024))(G_h)
+    # G_h = UpSampling2D(size=(2, 2))(G_h)
+    G_h = Activation('relu')(BatchNormalization()(Convolution2D(512, kernel_size=(5, 5), padding='same')(G_h)))
+    G_h = Activation('relu')(BatchNormalization()(Convolution2D(256, kernel_size=(5, 5), padding='same')(G_h)))
+    G_h = Activation('relu')(BatchNormalization()(Convolution2D(128, kernel_size=(5, 5), padding='same')(G_h)))
+    G_h = Activation('relu')(BatchNormalization()(Convolution2D(64, kernel_size=(5, 5), padding='same')(G_h)))
+    G_prob = Convolution2D(3, kernel_size=(1, 1), padding='same', activation='sigmoid')(G_h)
 
-    G = Model(inputs=[z_in, y_in], outputs=G_prob)
+    G = Model(inputs=[z_in, y_in], outputs=G_prob, name='Generator')
+    print('=== Generator ===')
     G.summary()
+    print('\n\n')
 
     return G
 
@@ -100,15 +89,15 @@ def plot(samples):
         ax.set_xticklabels([])
         ax.set_yticklabels([])
         ax.set_aspect('equal')
-        plt.imshow(sample.reshape(28, 28), cmap='Greys_r')
+        plt.imshow(sample.reshape(32, 32, 3), cmap='Greys_r')
 
     return fig
 
+
 """ Input placeholders """
-X = tf.placeholder(tf.float32, shape=[None, 28, 28, 1])
+X = tf.placeholder(tf.float32, shape=[None, 32, 32, 3])
 y = tf.placeholder(tf.float32, shape=[None, y_dim])
 Z = tf.placeholder(tf.float32, shape=[None, Z_dim])
-
 
 D_m = discriminator(X_dim, y_dim)
 theta_D = D_m.trainable_variables
@@ -120,14 +109,15 @@ G_sample = G_m([Z, y])
 D_logit_real = D_m([X, y])
 D_logit_fake = D_m([G_sample, y])
 
-D_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_logit_real, labels=tf.ones_like(D_logit_real)))
-D_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_logit_fake, labels=tf.zeros_like(D_logit_fake)))
+D_loss_real = tf.reduce_mean(
+    tf.nn.sigmoid_cross_entropy_with_logits(logits=D_logit_real, labels=tf.ones_like(D_logit_real)))
+D_loss_fake = tf.reduce_mean(
+    tf.nn.sigmoid_cross_entropy_with_logits(logits=D_logit_fake, labels=tf.zeros_like(D_logit_fake)))
 D_loss = D_loss_real + D_loss_fake
 G_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_logit_fake, labels=tf.ones_like(D_logit_fake)))
 
 D_solver = tf.train.AdamOptimizer().minimize(D_loss, var_list=theta_D)
 G_solver = tf.train.AdamOptimizer().minimize(G_loss, var_list=theta_G)
-
 
 with tf.device('/gpu:0'):
     sess = tf.Session()
@@ -136,31 +126,50 @@ with tf.device('/gpu:0'):
 if not os.path.exists('out/'):
     os.makedirs('out/')
 
+datagen = ImageDataGenerator(
+    featurewise_center=False,  # set input mean to 0 over the dataset
+    samplewise_center=False,  # set each sample mean to 0
+    featurewise_std_normalization=False,  # divide inputs by std of the dataset
+    samplewise_std_normalization=False,  # divide each input by its std
+    zca_whitening=False,  # apply ZCA whitening
+    rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+    width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+    height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+    horizontal_flip=True,  # randomly flip images
+    vertical_flip=False)  # randomly flip images  # Compute quantities required for feature-wise normalization
+# (std, mean, and principal components if ZCA whitening is applied).
+datagen.fit(x_train)
+
+gen = datagen.flow(x_train, y_train, batch_size=mb_size)
+
 i = 0
-n_sample = 16
 for it in range(1000000):
     if it % 1000 == 0:
-
+        n_sample = 16
         Z_sample = sample_Z(n_sample, Z_dim)
         y_sample = np.zeros(shape=[n_sample, y_dim])
         y_sample[range(n_sample), np.random.randint(0, 10, n_sample)] = 1
 
-        samples = sess.run(G_sample, feed_dict={Z: Z_sample, y:y_sample})
+        samples = sess.run(G_sample, feed_dict={Z: Z_sample, y: y_sample})
 
         fig = plot(samples)
         plt.savefig('out/{}.png'.format(str(i).zfill(3)), bbox_inches='tight')
         i += 1
         plt.close(fig)
 
-    X_mb, y_mb = mnist.train.next_batch(mb_size)
-    X_mb = X_mb.reshape(-1, 28, 28, 1)
+    # X_mb, y_mb = mnist.train.next_batch(mb_size)
+    # X_mb = X_mb.reshape(-1, 32, 32, 3)
+    X_mb, y_mb = next(gen)
+    # print('X_mb: {}'.format(X_mb.shape))
+    # print('y_mb: {}'.format(y_mb.shape))
+    y_mb = to_categorical(y_mb, num_classes=y_dim)
 
-    Z_sample = sample_Z(mb_size, Z_dim)
-    _, D_loss_curr = sess.run([D_solver, D_loss], feed_dict={X: X_mb, Z: Z_sample, y:y_mb})
-    _, G_loss_curr = sess.run([G_solver, G_loss], feed_dict={Z: Z_sample, y:y_mb})
+    Z_sample = sample_Z(X_mb.shape[0], Z_dim)
+    _, D_loss_curr = sess.run([D_solver, D_loss], feed_dict={X: X_mb, Z: Z_sample, y: y_mb})
+    _, G_loss_curr = sess.run([G_solver, G_loss], feed_dict={Z: Z_sample, y: y_mb})
 
     if it % 1000 == 0:
         print('Iter: {}'.format(it))
-        print('D loss: {:.4}'. format(D_loss_curr))
+        print('D loss: {:.4}'.format(D_loss_curr))
         print('G_loss: {:.4}'.format(G_loss_curr))
         print()
